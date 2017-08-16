fabb3af7b719c9df8277c1fd1dd9f1949f956312
rummykhan <rehan_manzoor@outlook.com>
2017-08-16 06:01:53 +0400

    Updated tests

--- tests/AppBundle/Controller/FeedControllerTest.php
+++ tests/AppBundle/Controller/FeedControllerTest.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FeedControllerTest extends WebTestCase
 {
-    public function testFeed()
+    public function testFeedResponse()
     {
         $client = static::createClient();
 
@@ -40,5 +40,31 @@ class FeedControllerTest extends WebTestCase
         $client->request('POST', '/feed', [], [], ['CONTENT_TYPE' => 'application/json'], $content);
 
         $this->assertEquals(422, $client->getResponse()->getStatusCode());
+    }
+
+    public function testFeedLimit()
+    {
+        $client = static::createClient();
+
+        $client->request('GET', '/', [], [], ['PHP_AUTH_USER' => 'admin', 'PHP_AUTH_PW' => 'admin']);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+
+        foreach ([10, 20, 50, 100] as $range) {
+
+            $content = json_encode([
+                'url' => 'http://pf.tradetracker.net/?aid=1&type=xml&encoding=utf-8&fid=251713&categoryType=2&additionalType=2',
+                'limit' => $range
+            ]);
+
+            $client->request('POST', '/feed', [], [], ['CONTENT_TYPE' => 'application/json'], $content);
+
+            $response = json_decode($client->getResponse()->getContent(), true);
+
+            $this->assertArrayHasKey('success', $response);
+            $this->assertArrayHasKey('data', $response);
+
+            $this->assertCount($range, $response['data']);
+        }
     }
 }
